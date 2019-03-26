ff7db5b53af3662318d4ff6ac4918aca16031453
Ross Jones <rossmosis.jones@gmail.com>
2019-03-26 10:55:45 -0400

    mural controller update

--- app/controllers/api/v1/murals_controller.rb
+++ app/controllers/api/v1/murals_controller.rb
@@ -12,6 +12,11 @@ class Api::V1::MuralsController < ApplicationController
     render json: Mural.create(mural_params)
   end
 
+  def update
+    Mural.find(params[:id]).update(mural_params)
+    render json: Mural.find(params[:id])
+  end
+
   private
 
   def mural_params
