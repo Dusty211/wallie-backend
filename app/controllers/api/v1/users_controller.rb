783b62ee29cce6d39d2eca5fbcd4e9da81ccf385
Dusty211 <kylebenz@hotmail.com>
2019-03-26 13:59:34 -0400

    finished with guide. pre-testing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::UsersController < ApplicationController
 
   skip_before_action :authorized, only: [:create]
 
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
   def index
     render json: User.all
   end
